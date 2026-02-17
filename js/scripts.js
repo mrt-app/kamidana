166c9bbf308904969f1699e49f22936d4cb16e6f
mrt-app <another.mrt@gmail.com>
2026-02-17 09:37:18 +0900

    [add] google tag

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,25 @@
 	location.href = './index-en.html';
 	}
 })();
+
+document.addEventListener('DOMContentLoaded', function() {
+  const storeBadges = document.querySelectorAll('.store-badge');
+  storeBadges.forEach(function(badge) {
+    badge.addEventListener('click', function() {
+      const url = badge.getAttribute('href');
+      let storeName = 'unknown';
+      if (url.includes('apple.com')) {
+        storeName = 'apple';
+      } else if (url.includes('google.com')) {
+        storeName = 'google';
+      }
+      
+      if (typeof gtag === 'function') {
+        gtag('event', 'click_store_badge', {
+          'store_name': storeName,
+          'destination_url': url
+        });
+      }
+    });
+  });
+});
